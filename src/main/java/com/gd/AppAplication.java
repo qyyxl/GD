4dbdbac03a33284dde6a4463dafd052a08d8984d
尹训岭 <17686748103@163.com>
2019-04-20 13:21:54 +0800

    first commit

---
+++ src/main/java/com/gd/AppAplication.java
@@ -0,0 +1,27 @@
+package com.gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+@ComponentScan("com.gd.*")
+@EnableScheduling
+@Controller
+public class AppAplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppAplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppAplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gd/common/WebConfig.java
@@ -0,0 +1,14 @@
+/*
+package com.gd.common;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class WebConfig {
+
+    @Value("${server.port}")
+    public String port;
+
+}
+*/
